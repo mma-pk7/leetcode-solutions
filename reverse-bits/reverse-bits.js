d5cdd7e57b52b344a147deb32a1e021f3795aa68
Muhammad Mahmood <76597754+mma-pk7@users.noreply.github.com>
2021-01-09 10:07:50 +0500

    Time: 92 ms (67.47%), Space: 40.3 MB (68.82%) - LeetHub

---
+++ reverse-bits/reverse-bits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+    var result = 0;
+    for (var i = 0; i < 32; i ++) {
+        result <<= 1;
+        if (n & 1 === 1) {
+            result += 1;
+        }
+        n >>= 1;
+    }
+    return result>>>0;
+};
